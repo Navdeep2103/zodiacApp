303bbcd3eda54c48412742c4ffcc0bf23832b54b
Navdeep Kaur <86navdeep.k@gmail.com>
2018-10-24 15:04:31 -0700

    initial commit

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/client'));
+// set enviroment variable
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log('Server is running on port ' + port);
