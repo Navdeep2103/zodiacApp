c86f23c7d512af81a7a367acd527b719e60cb7d0
Navdeep Kaur <86navdeep.k@gmail.com>
2018-10-26 13:19:45 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -2,16 +2,25 @@ var express = require('express');
 var key = process.env.api_key;
 var path = require('path');
  var app = express();
+ app.set('view engine', 'html');
 
 
 
-app.use(express.static(path.join(__dirname, '/client')));
 
 
 
 // set enviroment variable
 
 var port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, '/client')));
+app.set('view engine', 'html');
+
+
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
 
 app.listen(port, () => {
 	console.log('Server is running on port ' + port);
